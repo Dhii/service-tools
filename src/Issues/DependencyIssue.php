b48e5061b0fb1cd2b6ecc079659a7624f6dd3d9e
Miguel <miguelmuscat93@gmail.com>
2021-03-16 23:49:04 +0100

    Added strict types

--- src/Issues/DependencyIssue.php
+++ src/Issues/DependencyIssue.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dhii\Services\Tools\Issues;
 
 class DependencyIssue extends ServiceIssue
--- src/Utils/GetServiceDepsCapableTrait.php
+++ src/Utils/GetServiceDepsCapableTrait.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dhii\Services\Tools\Utils;
 
 use Dhii\Services\ServiceInterface;
